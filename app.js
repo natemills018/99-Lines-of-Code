eee1a565bdff0381e48960c145766df26d750fca
Nate Mills <natemills018@gmail.com>
2023-08-30 19:22:16 -0400

    Adding some improvements to the older code

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 let friends = ['Matt', 'Nick', 'Noelle', 'Roger', 'Jordan'];
 
 for (let a = 0; a < friends.length; a++) {
-    for (let b = 99; b >= 0; b--) {
-        if (b == 99) {
-            console.log(friends[a]);
-            console.log(b + " lines of code in file, " + b + " lines of code, " + friends[a] + ", strikes one out" + ", clears it all out, " + [b - 1] + ' lines of code in the file');
+    console.log(friends[a]);
+    for (let b = 99; b > 0; b--) {
+
+        if (b === 1) {
+            console.log(b + " line of code in file, " + b + " line of code, " + friends[a] + ", strikes one out" + ", clears it all out" + ", no more lines of code in the file");
+        } else if (b === 2) {
+            console.log(b + " lines of code in file, " + b + " lines of code, " + friends[a] + ", strikes one out" + ", clears it all out, " + [b - 1] + ' line of code in the file');
         } else {
             console.log(b + " lines of code in file, " + b + " lines of code, " + friends[a] + ", strikes one out" + ", clears it all out, " + [b - 1] + ' lines of code in the file');
-            if (b == 2) {
-                console.log("1 line of code in file, " + "1 line of code, " + friends[a] + ", strikes one out" + ", clears it all out" + ", no more lines of code in the file");
-                break;
-            }
         }
-
     }
 }
